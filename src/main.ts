fb40576565a12b7244cae488446f04950d8a5ccb
DanielBarros854 <91908602+DanielBarros854@users.noreply.github.com>
2022-08-17 17:38:21 -0300

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000, () =>
+    console.log('Server is running at: http://localhost:4000'),
+  );
+}
+bootstrap();
